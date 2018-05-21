ef9fcf68ac38009934871dfdb0c3dbd8bb648a3d
Amalie S <amaliesmidth@gmail.com>
2018-05-21 14:52:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var a
+var b 
+
+function add() {
+  
+}
+
+it('add(a, b) adds two numbers and returns the result', function() {
+  expect(add(a, b)).toEqual(a + b)
+})
+
+it('subtract(a, b) subtracts b from a and returns the result', function() {
+  expect(subtract(a, b)).toEqual(a - b)
+})
+
+it('multiply(a, b) multiplies two numbers and returns the result', function() {
+  expect(multiply(a, b)).toEqual(a * b)
+})
+
+it('divide(a, b) divides a by b and returns the result', function() {
+  expect(divide(a, b)).toEqual(a / b)
+})
